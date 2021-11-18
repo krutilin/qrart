05c10340cc4449afc1a2b4d6aba274006d38f162
alexlavrov <alex.s.lavrov@gmail.com>
2021-11-18 17:07:44 +0300

    Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import "nes.css/css/nes.min.css"
+import "nes.css/css/nes.min.css";
+import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,27 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const qrcode = require('qrcode-generator')
-const fs = require('fs')
-const { createCanvas, loadImage } = require('canvas')
+const qrcode = require("qrcode-generator");
+const fs = require("fs");
+const { createCanvas, loadImage } = require("canvas");
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(500).json({ error: 'Method not allowed' })
+  if (req.method !== "POST") {
+    return res.status(500).json({ error: "Method not allowed" });
   }
 
-  const { data } = req.body
+  const { data } = req.body;
 
-  const canvas = createCanvas(320, 320)
-  const ctx = canvas.getContext('2d')
+  const canvas = createCanvas(320, 320);
+  const ctx = canvas.getContext("2d");
 
-  const QR = qrcode(6, 'L')
-  QR.addData(data)
-  QR.make()
+  const QR = qrcode(6, "L");
+  QR.addData(data);
+  QR.make();
 
-  const image = await loadImage('public/download.png')
-  ctx.drawImage(image, 0, 0)
+  const image = await loadImage("public/download.png");
+  ctx.drawImage(image, 0, 0);
 
   var pixelSize = 1;
-  var blockSize = (3 * pixelSize);
+  var blockSize = 3 * pixelSize;
 
   var pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
   var d = pixels.data;
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
     var r = d[i];
     var g = d[i + 1];
     var b = d[i + 2];
-    var grey = (r * 0.2126 + g * 0.7152 + b * 0.0722);
+    var grey = r * 0.2126 + g * 0.7152 + b * 0.0722;
     //var v = (grey >= 127) ? 255 : 0;
     //d[i] = d[i+1] = d[i+2] = v;
     d[i] = d[i + 1] = d[i + 2] = grey;
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
 
   for (var i = 0; i < d.length; i += 4) {
     var grey = d[i];
-    var v = (grey >= 127) ? 255 : 0;
+    var v = grey >= 127 ? 255 : 0;
 
     // Dithering
     var error = (grey - v) / 8;
@@ -48,14 +48,32 @@ export default async function handler(req, res) {
 
     d[i] = d[i + 1] = d[i + 2] = v;
 
-    d[(((row + 0) * width) + (cell + 1)) * 4] = d[(((row + 0) * width) + (cell + 1)) * 4 + 1] = d[(((row + 0) * width) + (cell + 1)) * 4 + 2] = d[(((row + 0) * width) + (cell + 1)) * 4] + error;
-    d[(((row + 0) * width) + (cell + 2)) * 4] = d[(((row + 0) * width) + (cell + 2)) * 4 + 1] = d[(((row + 0) * width) + (cell + 2)) * 4 + 2] = d[(((row + 0) * width) + (cell + 2)) * 4] + error;
-    d[(((row + 1) * width) + (cell - 1)) * 4] = d[(((row + 1) * width) + (cell - 1)) * 4 + 1] = d[(((row + 1) * width) + (cell - 1)) * 4 + 2] = d[(((row + 1) * width) + (cell - 1)) * 4] + error;
-    d[(((row + 1) * width) + (cell + 0)) * 4] = d[(((row + 1) * width) + (cell + 0)) * 4 + 1] = d[(((row + 1) * width) + (cell + 0)) * 4 + 2] = d[(((row + 1) * width) + (cell + 0)) * 4] + error;
-    d[(((row + 1) * width) + (cell + 1)) * 4] = d[(((row + 1) * width) + (cell + 1)) * 4 + 1] = d[(((row + 1) * width) + (cell + 1)) * 4 + 2] = d[(((row + 1) * width) + (cell + 1)) * 4] + error;
-    d[(((row + 2) * width) + (cell + 0)) * 4] = d[(((row + 2) * width) + (cell + 0)) * 4 + 1] = d[(((row + 2) * width) + (cell + 0)) * 4 + 2] = d[(((row + 2) * width) + (cell + 0)) * 4] + error;
+    d[((row + 0) * width + (cell + 1)) * 4] =
+      d[((row + 0) * width + (cell + 1)) * 4 + 1] =
+      d[((row + 0) * width + (cell + 1)) * 4 + 2] =
+        d[((row + 0) * width + (cell + 1)) * 4] + error;
+    d[((row + 0) * width + (cell + 2)) * 4] =
+      d[((row + 0) * width + (cell + 2)) * 4 + 1] =
+      d[((row + 0) * width + (cell + 2)) * 4 + 2] =
+        d[((row + 0) * width + (cell + 2)) * 4] + error;
+    d[((row + 1) * width + (cell - 1)) * 4] =
+      d[((row + 1) * width + (cell - 1)) * 4 + 1] =
+      d[((row + 1) * width + (cell - 1)) * 4 + 2] =
+        d[((row + 1) * width + (cell - 1)) * 4] + error;
+    d[((row + 1) * width + (cell + 0)) * 4] =
+      d[((row + 1) * width + (cell + 0)) * 4 + 1] =
+      d[((row + 1) * width + (cell + 0)) * 4 + 2] =
+        d[((row + 1) * width + (cell + 0)) * 4] + error;
+    d[((row + 1) * width + (cell + 1)) * 4] =
+      d[((row + 1) * width + (cell + 1)) * 4 + 1] =
+      d[((row + 1) * width + (cell + 1)) * 4 + 2] =
+        d[((row + 1) * width + (cell + 1)) * 4] + error;
+    d[((row + 2) * width + (cell + 0)) * 4] =
+      d[((row + 2) * width + (cell + 0)) * 4 + 1] =
+      d[((row + 2) * width + (cell + 0)) * 4 + 2] =
+        d[((row + 2) * width + (cell + 0)) * 4] + error;
   }
   ctx.putImageData(pixels, 0, 0);
 
-  res.status(200).json({ url: /*canvas.toDataURL()*/ QR.createDataURL(4, 2) })
+  res.status(200).json({ url: QR.createDataURL(4, 2) });
 }
