faeccf4c88f004acaba63d496c9b6f1c94c4ca63
Yuriy Krutilin <y.krutilin@gmail.com>
2021-11-19 16:50:48 +0300

    footer

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
           */}
         </div>
 
-        <p><span>©2021</span> <a href="https://github.com/alexslavr" target="_blank" rel="noopener noreferrer">@alexslavr</a> <span> </span> <a href="https://github.com/krutilin" target="_blank" rel="noopener noreferrer">@krutilin</a></p>
+        <footer><span>©2021</span> <a href="https://github.com/alexslavr" target="_blank" rel="noopener noreferrer">@alexslavr</a> <span> </span> <a href="https://github.com/krutilin" target="_blank" rel="noopener noreferrer">@krutilin</a></footer>
       </main>
 
     </div>
