2a4b760aa43a5ac60e5d99a78f6dbeefe1dcd92b
Yuriy Krutilin <y.krutilin@gmail.com>
2021-11-19 20:26:26 +0300

    Add app icon

--- pages/index.js
+++ pages/index.js
@@ -87,6 +87,10 @@ export default function Home({ texts }) {
           href="https://fonts.googleapis.com/css?family=Press+Start+2P"
           rel="stylesheet"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className={styles.main}>
         <header className={styles.header}>
