eaf2ff2cee72d90933ccd94bdfb041ee5504082b
Yuriy Krutilin <y.krutilin@gmail.com>
2021-11-19 17:22:53 +0300

    Add minimal localisation

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,24 @@ import styles from "../styles/Home.module.css";
 
 const texts = {
   en: {
-    title: "",
+    title: "QRcode pixel ART generator",
+    description: "Generate cool pixel art instead of boring QR code, make so-called halftone QR codes in a fun way",
+    h1: "QR ART generator",
+    p: "Generate cool pixel art instead of boring QR code",
+    h3_title: "Your data",
+    h4: "URL or text",
+    button: "Generate",
+    qr: "Your QR code"
   },
   ru: {
-    title: "",
+    title: "QRcode пиксель ART генератор",
+    description: "Создавайте крутые пиксельные изображения вместо скучных QR-кодов, так называемые полутоновые QR-коды.",
+    h1: "QR ART генератор",
+    p: "Создавайте крутые пиксельные рисунки вместо скучного QR-кода",
+    h3_title: "Введите данные",
+    h4: "URL или текст",
+    button: "Генерируй!",
+    qr: "Твой QR код"
   },
 };
 
@@ -60,10 +74,10 @@ export default function Home({ texts }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>QRcode pixel ART generator</title>
+        <title>{texts.title}</title>
         <meta
           name="description"
-          content="Generate cool pixel art instead of boring QR code, make so-called halftone QR codes in a fun way"
+          content={texts.description}
         />
         <link rel="icon" href="/favicon.ico" />
         <link
@@ -74,28 +88,29 @@ export default function Home({ texts }) {
 
       <main className={styles.main}>
         <header className={styles.header}>
-          <h1>QR ART generator</h1>
+          <h1>{texts.h1}</h1>
         </header>
 
         <div className={styles.content}>
           <section className="message-left">
             <i className="nes-bcrikko"></i>
             <p className="nes-balloon nes-pointer from-left">
-              Generate cool pixel art instead of boring QR code
+              {texts.p}
             </p>
           </section>
 
           <section className="nes-container with-title section">
-            <h3 className="title">Your data</h3>
+            <h3 className="title">{texts.h3_title}</h3>
+            <h4>{texts.h4}</h4>
             <input ref={input} type="text" className="nes-input" />
             <button className="nes-btn is-primary" onClick={sendData}>
-              Generate
+              {texts.button}
             </button>
           </section>
 
           {url && (
             <section ref={code} className="nes-container with-title section">
-              <h3 className="title">Your QR code</h3>
+              <h3 className="title">{texts.qr}</h3>
               <div className="code-container animate__animated animate__bounceInRight">
                 <img className="qr" src={url} alt="QR" />
               </div>
