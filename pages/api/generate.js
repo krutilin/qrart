f111df861112b900922dc47718ad06a1abb1e3b7
alexlavrov <alex.s.lavrov@gmail.com>
2021-11-21 15:36:26 +0300

    Catch errors on server

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -22,151 +22,156 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Method not allowed" });
   }
 
-  const { data, index, file } = req.body;
+  try {
+    const { data, index, file } = req.body;
 
-  const imgSize = 512;
+    const imgSize = 512;
 
-  const QR = qrcode(6, "M");
-  QR.addData(data);
-  QR.make();
+    const QR = qrcode(6, "M");
+    QR.addData(data);
+    QR.make();
 
-  const rndImageIndex =
-    index != null ? index : Math.floor(Math.random() * images.length);
+    const rndImageIndex =
+      index != null ? index : Math.floor(Math.random() * images.length);
 
-  let rndImageUrl = `${cdn_url}${images[rndImageIndex]}.png`;
+    let rndImageUrl = `${cdn_url}${images[rndImageIndex]}.png`;
 
-  if (file) {
-    rndImageUrl = file;
-  }
+    if (file) {
+      rndImageUrl = file;
+    }
 
-  let pixelSize = 1;
-  let blockSize = Math.floor((pixelSize * imgSize) / QR.getModuleCount()); // 3 * pixelSize;
+    let pixelSize = 1;
+    let blockSize = Math.floor((pixelSize * imgSize) / QR.getModuleCount()); // 3 * pixelSize;
 
-  const canvasSize = blockSize * QR.getModuleCount();
+    const canvasSize = blockSize * QR.getModuleCount();
 
-  const canvas = createCanvas(canvasSize, canvasSize);
-  const ctx = canvas.getContext("2d");
+    const canvas = createCanvas(canvasSize, canvasSize);
+    const ctx = canvas.getContext("2d");
 
-  const image = await loadImage(rndImageUrl);
-  ctx.drawImage(image, 0, 0, canvasSize, canvasSize);
+    const image = await loadImage(rndImageUrl);
+    ctx.drawImage(image, 0, 0, canvasSize, canvasSize);
 
-  var pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  var d = pixels.data;
-  var width = Math.sqrt(d.length / 4) / pixelSize;
-  for (var i = 0; i < d.length; i += 4) {
-    var r = d[i];
-    var g = d[i + 1];
-    var b = d[i + 2];
-    var grey = r * 0.2126 + g * 0.7152 + b * 0.0722;
-    //var v = (grey >= 127) ? 255 : 0;
-    //d[i] = d[i+1] = d[i+2] = v;
-    d[i] = d[i + 1] = d[i + 2] = grey;
-  }
+    var pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var d = pixels.data;
+    var width = Math.sqrt(d.length / 4) / pixelSize;
+    for (var i = 0; i < d.length; i += 4) {
+      var r = d[i];
+      var g = d[i + 1];
+      var b = d[i + 2];
+      var grey = r * 0.2126 + g * 0.7152 + b * 0.0722;
+      //var v = (grey >= 127) ? 255 : 0;
+      //d[i] = d[i+1] = d[i+2] = v;
+      d[i] = d[i + 1] = d[i + 2] = grey;
+    }
 
-  for (var i = 0; i < d.length; i += 4) {
-    var grey = d[i];
-    var v = grey >= 127 ? 255 : 0;
+    for (var i = 0; i < d.length; i += 4) {
+      var grey = d[i];
+      var v = grey >= 127 ? 255 : 0;
 
-    // Dithering
-    var error = (grey - v) / 8;
-    var i2 = i / 4;
-    var row = Math.floor(i2 / width);
-    var cell = i2 % width;
+      // Dithering
+      var error = (grey - v) / 8;
+      var i2 = i / 4;
+      var row = Math.floor(i2 / width);
+      var cell = i2 % width;
 
-    d[i] = d[i + 1] = d[i + 2] = v;
+      d[i] = d[i + 1] = d[i + 2] = v;
 
-    d[((row + 0) * width + (cell + 1)) * 4] =
-      d[((row + 0) * width + (cell + 1)) * 4 + 1] =
-      d[((row + 0) * width + (cell + 1)) * 4 + 2] =
-        d[((row + 0) * width + (cell + 1)) * 4] + error;
-    d[((row + 0) * width + (cell + 2)) * 4] =
-      d[((row + 0) * width + (cell + 2)) * 4 + 1] =
-      d[((row + 0) * width + (cell + 2)) * 4 + 2] =
-        d[((row + 0) * width + (cell + 2)) * 4] + error;
-    d[((row + 1) * width + (cell - 1)) * 4] =
-      d[((row + 1) * width + (cell - 1)) * 4 + 1] =
-      d[((row + 1) * width + (cell - 1)) * 4 + 2] =
-        d[((row + 1) * width + (cell - 1)) * 4] + error;
-    d[((row + 1) * width + (cell + 0)) * 4] =
-      d[((row + 1) * width + (cell + 0)) * 4 + 1] =
-      d[((row + 1) * width + (cell + 0)) * 4 + 2] =
-        d[((row + 1) * width + (cell + 0)) * 4] + error;
-    d[((row + 1) * width + (cell + 1)) * 4] =
-      d[((row + 1) * width + (cell + 1)) * 4 + 1] =
-      d[((row + 1) * width + (cell + 1)) * 4 + 2] =
-        d[((row + 1) * width + (cell + 1)) * 4] + error;
-    d[((row + 2) * width + (cell + 0)) * 4] =
-      d[((row + 2) * width + (cell + 0)) * 4 + 1] =
-      d[((row + 2) * width + (cell + 0)) * 4 + 2] =
-        d[((row + 2) * width + (cell + 0)) * 4] + error;
-  }
-  ctx.putImageData(pixels, 0, 0);
+      d[((row + 0) * width + (cell + 1)) * 4] =
+        d[((row + 0) * width + (cell + 1)) * 4 + 1] =
+        d[((row + 0) * width + (cell + 1)) * 4 + 2] =
+          d[((row + 0) * width + (cell + 1)) * 4] + error;
+      d[((row + 0) * width + (cell + 2)) * 4] =
+        d[((row + 0) * width + (cell + 2)) * 4 + 1] =
+        d[((row + 0) * width + (cell + 2)) * 4 + 2] =
+          d[((row + 0) * width + (cell + 2)) * 4] + error;
+      d[((row + 1) * width + (cell - 1)) * 4] =
+        d[((row + 1) * width + (cell - 1)) * 4 + 1] =
+        d[((row + 1) * width + (cell - 1)) * 4 + 2] =
+          d[((row + 1) * width + (cell - 1)) * 4] + error;
+      d[((row + 1) * width + (cell + 0)) * 4] =
+        d[((row + 1) * width + (cell + 0)) * 4 + 1] =
+        d[((row + 1) * width + (cell + 0)) * 4 + 2] =
+          d[((row + 1) * width + (cell + 0)) * 4] + error;
+      d[((row + 1) * width + (cell + 1)) * 4] =
+        d[((row + 1) * width + (cell + 1)) * 4 + 1] =
+        d[((row + 1) * width + (cell + 1)) * 4 + 2] =
+          d[((row + 1) * width + (cell + 1)) * 4] + error;
+      d[((row + 2) * width + (cell + 0)) * 4] =
+        d[((row + 2) * width + (cell + 0)) * 4 + 1] =
+        d[((row + 2) * width + (cell + 0)) * 4 + 2] =
+          d[((row + 2) * width + (cell + 0)) * 4] + error;
+    }
+    ctx.putImageData(pixels, 0, 0);
 
-  const cornerSize = 7;
+    const cornerSize = 7;
 
-  const isCorner = (row, col, count) => {
-    /*var pos =
-      2 [6, 18],
-      3 [6, 22],
-      4 [6, 26],
-      5 [6, 30],
-      6 [6, 34],
-      */
+    const isCorner = (row, col, count) => {
+      /*var pos =
+        2 [6, 18],
+        3 [6, 22],
+        4 [6, 26],
+        5 [6, 30],
+        6 [6, 34],
+        */
 
-    // adjustPattern
-    let pos = [6, 34];
+      // adjustPattern
+      let pos = [6, 34];
 
-    return (
-      (row < 7 && col < 7) ||
-      (row < 7 && col >= count - 7) ||
-      (col < 7 && row >= count - 7) ||
-      col === 6 ||
-      row === 6 ||
-      (col < 34 + 3 && col > 34 - 3 && row < 34 + 3 && row > 34 - 3)
-    );
-  };
+      return (
+        (row < 7 && col < 7) ||
+        (row < 7 && col >= count - 7) ||
+        (col < 7 && row >= count - 7) ||
+        col === 6 ||
+        row === 6 ||
+        (col < 34 + 3 && col > 34 - 3 && row < 34 + 3 && row > 34 - 3)
+      );
+    };
 
-  for (let byteRow = 0; byteRow < QR.getModuleCount(); byteRow++) {
-    for (let byteCell = 0; byteCell < QR.getModuleCount(); byteCell++) {
-      // Middle Cell
-      ctx.fillStyle = ctx.strokeStyle = QR.isDark(byteRow, byteCell)
-        ? "black"
-        : "white";
+    for (let byteRow = 0; byteRow < QR.getModuleCount(); byteRow++) {
+      for (let byteCell = 0; byteCell < QR.getModuleCount(); byteCell++) {
+        // Middle Cell
+        ctx.fillStyle = ctx.strokeStyle = QR.isDark(byteRow, byteCell)
+          ? "black"
+          : "white";
 
-      if (isCorner(byteRow, byteCell, QR.getModuleCount())) {
-        ctx.fillRect(
-          byteRow * blockSize + pixelSize,
-          byteCell * blockSize + pixelSize,
-          blockSize,
-          blockSize
-        );
-      } else {
-        ctx.beginPath();
-        ctx.arc(
-          byteRow * blockSize + blockSize / 2,
-          byteCell * blockSize + blockSize / 2,
-          Math.round(blockSize / 6),
-          0,
-          Math.PI * 2
-        );
-        ctx.fill();
-        ctx.stroke();
+        if (isCorner(byteRow, byteCell, QR.getModuleCount())) {
+          ctx.fillRect(
+            byteRow * blockSize + pixelSize,
+            byteCell * blockSize + pixelSize,
+            blockSize,
+            blockSize
+          );
+        } else {
+          ctx.beginPath();
+          ctx.arc(
+            byteRow * blockSize + blockSize / 2,
+            byteCell * blockSize + blockSize / 2,
+            Math.round(blockSize / 6),
+            0,
+            Math.PI * 2
+          );
+          ctx.fill();
+          ctx.stroke();
+        }
       }
     }
+
+    const buffer = canvas.toBuffer();
+    const key = `res/${nanoid(10)}/qrcode.${fileExtension}`;
+
+    const params = {
+      Body: buffer,
+      Bucket: BUCKET,
+      Key: key,
+      ACL: "public-read",
+    };
+
+    await s3.putObject(params).promise();
+    const url = `https://${BUCKET}.${ENDPOINT}/${key}`;
+
+    res.status(200).json({ url });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Internal server error!" });
   }
-
-  const buffer = canvas.toBuffer();
-  const key = `res/${nanoid(10)}/qrcode.${fileExtension}`;
-
-  const params = {
-    Body: buffer,
-    Bucket: BUCKET,
-    Key: key,
-    ACL: "public-read",
-  };
-
-  await s3.putObject(params).promise();
-  const url = `https://${BUCKET}.${ENDPOINT}/${key}`;
-
-  res.status(200).json({ url });
 }
